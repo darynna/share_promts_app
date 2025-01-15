1cb98448a8c1ecc6bec8b125a2f2d1413db35039
Daryna Holikova <dgolikova0@gmail.com>
2025-01-15 13:57:07 +0000

    add new components

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+"use client";
+
+import { useState, useEffect } from "react";
+import PromtCart from "./PromtCart";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => {
+        return (
+          <PromtCart
+            key={post._id}
+            post={post}
+            handleTagClick={handleTagClick}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default function Feed() {
+  const [searchtext, setsearchtext] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  const handleSearcgChange = (e) => {};
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setPosts(data);
+    };
+
+    fetchPost();
+  }, []);
+
   return (
-    <div>
-     Feed
-    </div>
-  )
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or an username"
+          value={searchtext}
+          onChange={handleSearcgChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
 }
