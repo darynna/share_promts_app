24306741b7cdcf7999023d5247a1bba3a6f377a6
Daryna Holikova <dgolikova0@gmail.com>
2025-01-15 14:54:10 +0000

    and profile page and it elements

---
+++ app/profile/page.jsx
@@ -0,0 +1,36 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+export default function MyProfile() {
+  const { data: session } = useSession();
+
+  const [myPosts, setMyPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setMyPosts(data);
+    };
+
+    if (session?.user.id) fetchPost();
+  }, [session?.user.id]);
+
+  const handleEdit = () => {};
+
+  const handleDelete = async () => {};
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={myPosts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+}
